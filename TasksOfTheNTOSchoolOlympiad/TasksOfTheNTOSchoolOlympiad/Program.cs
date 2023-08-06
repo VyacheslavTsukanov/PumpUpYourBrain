ade5a0969377aacd4f38776fe0715ab106fe6c72
VyacheslavTsukanov <vtsukanoff@yandex.ru>
2023-08-06 21:52:19 +0400

    A new project has been created - TasksOfTheNTOSchoolOlympiad

---
+++ TasksOfTheNTOSchoolOlympiad/TasksOfTheNTOSchoolOlympiad/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TasksOfTheNTOSchoolOlympiad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
