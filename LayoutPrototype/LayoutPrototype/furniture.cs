56124d01424813138e4c7394818721673e5d2732
VyacheslavTsukanov <vtsukanoff@yandex.ru>
2023-08-06 21:50:55 +0400

    A new project has been created - LayoutPrototype

---
+++ LayoutPrototype/LayoutPrototype/furniture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LayoutPrototype
+{
+    class furniture // Высчитывает площадь мебели
+    {
+
+        public double wid;
+        public double len;
+
+        public double Print()
+        {
+            return wid * len;
+        }
+    }
+}
