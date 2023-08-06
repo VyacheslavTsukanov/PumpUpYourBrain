a319d82c97ef92cff9b5b127cc250cb39569b18f
VyacheslavTsukanov <115897591+VyacheslavTsukanov@users.noreply.github.com>
2023-08-06 20:54:14 +0400

    Add files via upload
    Solving mathematical problems Shape, Triangle

---
+++ SpaceAppTask1/CalculateTheAreaTest/Circle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CalculateTheAreaTest
+{
+    public class Circle : Shape
+    {
+        double radius;
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+        public override double Square() => Math.PI * radius * radius;
+    }
+}
---
+++ SpaceAppTask1/CalculateTheAreaTest/Triangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CalculateTheAreaTest
+{
+    public class Triangle : Shape
+    {
+        private readonly double _firstSide;
+        private readonly double _secondSide;
+        private readonly double _thirdSide;
+
+        public Triangle(double firstSide, double secondSide, double thirdSide)
+        {
+            _firstSide = firstSide;
+            _secondSide = secondSide;
+            _thirdSide = thirdSide;
+        }
+        public override double Square()
+        {
+            double semiPerimeter = (_firstSide + _secondSide + _thirdSide) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - _firstSide) * (semiPerimeter - _secondSide) * (semiPerimeter - _thirdSide));
+
+        }
+        public bool IsRight()
+        {
+            return ((Math.Pow(_firstSide, 2) + Math.Pow(_secondSide, 2)) == Math.Pow(_thirdSide, 2)) ||
+                    ((Math.Pow(_thirdSide, 2) + Math.Pow(_secondSide, 2)) == Math.Pow(_firstSide, 2)) ||
+                    ((Math.Pow(_firstSide, 2) + Math.Pow(_thirdSide, 2)) == Math.Pow(_secondSide, 2));
+        }
+    }
+}
